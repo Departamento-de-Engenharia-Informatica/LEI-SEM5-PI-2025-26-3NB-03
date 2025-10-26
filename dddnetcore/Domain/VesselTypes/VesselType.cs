9b7ed8ff1087ef4974931692c40aca19e983f4bf
nmcesar <1010947@isep.ipp.pt>
2025-10-26 22:55:39 +0000

    Add files via upload

--- dddnetcore/Domain/VesselTypes/VesselType.cs
+++ dddnetcore/Domain/VesselTypes/VesselType.cs
@@ -1,5 +1,8 @@
 using System;
 using DDDSample1.Domain.Shared;
+using DDDSample1.Domain.Docks;
+using System.Collections.Generic;
+
 
 namespace DDDSample1.Domain.VesselTypes
 {
@@ -12,10 +15,13 @@ namespace DDDSample1.Domain.VesselTypes
         public int MaxBays { get; private set; }
         public int MaxTiers { get; private set; }
 
+        private readonly List<Dock> _docks = new();
+        public IReadOnlyCollection<Dock> Docks => _docks.AsReadOnly();
+
         // EF Core
         private VesselType() { }
 
-        public VesselType(string name, string description, int capacity, int maxRows, int maxBays, int maxTiers)
+        public VesselType(string name, string description, int capacity, int maxrows, int maxbays, int maxtiers)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new BusinessRuleValidationException("Name is required.");
@@ -26,22 +32,25 @@ namespace DDDSample1.Domain.VesselTypes
             if (capacity <= 0)
                 throw new BusinessRuleValidationException("Capacity must be a positive integer.");
 
-            if (maxRows <= 0)
+            if (maxrows <= 0)
                 throw new BusinessRuleValidationException("MaxRows must be a positive integer.");
 
-            if (maxBays <= 0)
+            if (maxbays <= 0)
                 throw new BusinessRuleValidationException("MaxBays must be a positive integer.");
 
-            if (maxTiers <= 0)
+            if (maxtiers <= 0)
                 throw new BusinessRuleValidationException("MaxTiers must be a positive integer.");
 
             this.Id = new VesselTypeId(Guid.NewGuid());
             this.Name = name;
             this.Description = description;
             this.Capacity = capacity;
-            this.MaxRows = maxRows;
-            this.MaxBays = maxBays;
-            this.MaxTiers = maxTiers;
+            this.MaxRows = maxrows;
+            this.MaxBays = maxbays;
+            this.MaxTiers = maxtiers;
         }
+        
+       
+       
     }
 }
---
+++ dddnetcore/Domain/VesselTypes/VesselTypeDto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DDDSample1.Domain.VesselTypes
+{
+    public class VesselTypeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public int MaxRows { get; set; }
+        public int MaxBays { get; set; }
+        public int MaxTiers { get; set; }
+    }
+}
