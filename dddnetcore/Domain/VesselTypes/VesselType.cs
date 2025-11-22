11058d5dfbb578688219bc991cbb166edc7236c4
nmcesar <1010947@isep.ipp.pt>
2025-11-22 20:23:00 +0000

    Fix VesselTypeUpdate (Working)
    Adicionado update ao vessel type

--- dddnetcore/Domain/VesselTypes/VesselType.cs
+++ dddnetcore/Domain/VesselTypes/VesselType.cs
@@ -50,7 +50,37 @@ namespace DDDSample1.Domain.VesselTypes
             this.MaxTiers = maxtiers;
         }
         
-       
-       
+        public void ChangeName(string name)
+        {
+            this.Name = name;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            this.Description = description; 
+        }
+
+        public void ChangeCapacity(int capacity)
+        {
+            this.Capacity = capacity;
+        }
+
+        public void ChangeMaxBays(int maxbays)
+        {
+            this.MaxBays = maxbays;
+        }
+
+        public void ChangeMaxRows(int maxrows)
+        {
+            this.MaxRows = maxrows;
+        }
+
+        public void ChangeMaxTiers(int maxtiers)
+        {
+            this.MaxTiers = maxtiers;
+        }
+    
     }
+
+
 }
