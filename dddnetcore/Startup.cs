4de1ecd770ae9054180bc300b427e7bb3a9cec12
nmcesar <1010947@isep.ipp.pt>
2025-11-23 16:19:37 +0000

    DockChangesto3D
    Alterações às Docks para localização necessária para a visualização 3D e criação do método get all que estava em falta (não foi necessário para o sprint A)

--- dddnetcore/Startup.cs
+++ dddnetcore/Startup.cs
@@ -139,14 +139,18 @@ namespace DDDSample1
                 }
                 if (!ctx.VesselTypes.Any())
                 {
-                    ctx.VesselTypes.AddRange(new[] { new VesselType("Type1", "Desc1", 27, 3, 3, 3) });
+                    ctx.VesselTypes.AddRange(new[] { new VesselType("Type1", "Carqueiro Pequeno", 27, 3, 3, 3) });
+                    ctx.VesselTypes.AddRange(new[] { new VesselType("Type2", "Carqueiro Pequeno", 64, 4, 4, 4) });
                     ctx.SaveChanges();
                 }
+
                 if (!ctx.Docks.Any())
                 {
                     var dock1 = new Dock(
                         name: "Dock1",
-                        location: "Lateral Norte",
+                        locationx: 1,
+                        locationz: 1,
+                        locationorientation: 1,
                         length: 200,
                         depth: 15,
                         maxDraft: 12,
