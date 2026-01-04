36db8fa7bd5a8782bc0d2eea24be0e826526762e
Joao <1200614@isep.ipp.pt>
2026-01-04 22:50:52 +0000

    feat(backend): implement incident management module (CRUD) for US 4.1.13

--- schedule-backend/src/api/routes/incidentRoute.ts
+++ schedule-backend/src/api/routes/incidentRoute.ts
@@ -9,12 +9,12 @@ export default (app: Router) => {
 
     const ctrl = Container.get(IncidentController);
 
-    // Criar
+
     route.post('', (req, res, next) => ctrl.create(req, res, next));
 
-    // Listar (com filtros)
+
     route.get('', (req, res, next) => ctrl.list(req, res, next));
 
-    // Atualizar (Resolver ou Editar)
+
     route.patch('/:id', (req, res, next) => ctrl.update(req, res, next));
 };
